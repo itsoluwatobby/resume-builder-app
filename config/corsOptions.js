7faa1b93f2ca344d873214925d6c12cc20731948
itsoluwatobby <oluwasamuel050@gmail.com>
2022-11-06 08:54:09 +0100

    project completed

--- config/corsOptions.js
+++ config/corsOptions.js
@@ -1,8 +1,8 @@
-const allowedOrigins = ['https://resume-builder-tpvd.onrender.com/']
+const allowedOrigins = ['https://resume-builder-tpvd.onrender.com']
 
 const corsOptions = {
    origin: (origin, callback) => {
-      allowedOrigins.includes(origin) ? callback(null, true) : callback(new Error('NOT ALLOWED BY CORS'))
+      allowedOrigins.includes(origin) || !origin ? callback(null, true) : callback(new Error('NOT ALLOWED BY CORS'))
    },
    credentials: true,
    optionsSuccessStatus: 200
